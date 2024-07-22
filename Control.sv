3544b11d2d0197ab2ff5a075b7cb0affdcdd01fb
chedwards492 <chedwards492@gmail.com>
2024-07-22 16:41:48 -0700

    Control nearly done, waiting on ALU Opcodes

--- Control.sv
+++ Control.sv
@@ -3,7 +3,9 @@ module Control #(parameter opwidth = 3, mcodebits = 4)(
   input [mcodebits-1:0] instr,    // subset of machine code (any width you need)
   output logic RegDst, Branch, 
      MemtoReg, MemWrite, ALUSrc, RegWrite,
-  output logic[opwidth-1:0] ALUOp);	   // for up to 8 ALU operations
+  output logic[opwidth-1:0] ALUOp
+  output logic UncondJump, RdMem, WrMem, JType, IType, RegWrite, Movf
+);	   // for up to 8 ALU operations
 
 always_comb begin
 // defaults
@@ -13,7 +15,18 @@ always_comb begin
   ALUSrc 	=	'b0;   // 1: immediate  0: second reg file output
   RegWrite  =	'b1;   // 0: for store or no op  1: most other operations 
   MemtoReg  =	'b0;   // 1: load -- route memory instead of ALU to reg_file data in
+  
+  UncdJmp	=   'b0;
+  RdMem		=	'b0;
+  WrMem		= 	'b0;
+  JType		=	'b0;
+  IType		=	'b0;
+  RegWrite	=	'b1;
+  Movf		=	'b0;
   ALUOp	    =   'b111; // y = a+0;
+  
+  
+  
 // sample values only -- use what you need
 case(instr)    // override defaults with exceptions
   'b0000:  begin					// store operation
@@ -25,6 +38,67 @@ case(instr)    // override defaults with exceptions
 			   MemtoReg = 'b1;    // 
              end
 // ...
+  
+  // Unconditional jump
+  'b0000:
+  	begin
+      UncdJmp = 'b1;
+      JType = 'b1;
+      RegWrite = 'b0; // RegWrite disable on jumps
+    end
+  
+  // Jumps
+  'b0001,
+  'b0010,
+  'b0011,
+  'b0100:
+    begin
+      JType = 'b1;
+      RegWrite = 'b0;	// RegWrite disable on jumps
+    end
+  
+  // Ld
+  'b1000: RdMem = 'b1; 	// Read memory only on loads
+  
+  // Str
+  'b0111: 
+    begin
+      WrMem = 'b1;		// Write memory only on stores
+      RegWrite = 'b0;	// RegWrite disable on stores
+    end
+  
+  // Lsl
+  'b1101:
+    begin
+      IType = 'b1;
+      ALUOp = 'b0; // ************ ALU OP ******************
+    end
+    
+  // Movi
+  'b1110: IType = 'b1;
+  
+  // Cmp
+  'b1100: 
+    begin
+      RegWrite = 'b0; // RegWrite disable on cmp
+      ALUOp = 'b0; // subtraction ************ ALU OP ******************
+  
+  // Movf
+  'b1010: Movf = 'b1;
+      
+  // ************ ALU OPs ******************
+      
+  // Add
+  'b0101: ALUOp = 'b0;
+       
+  // Sub
+  'b0111: ALUOp = 'b0;
+  
+  // XOR
+  'b0110: ALUOp = 'b0;
+      
+  // 
+  
 endcase
 
 end
