49c199b25e20f210c5868c381b22848c438e6006
Christopher Edwards <66543275+chedwards492@users.noreply.github.com>
2024-07-24 19:23:12 -0700

    Re-added my files

---
+++ alu.sv
@@ -0,0 +1,39 @@
+// combinational -- no clock
+// sample -- change as desired
+module alu(
+  input[2:0] alu_cmd,    // ALU instructions
+  input[8:0] inA, inB,	 // 9-bit wide data path
+  output logic[8:0] rslt,
+  output logic flag		 // jump flag
+);
+
+always_comb begin 
+  rslt =  'b0;
+  flag =  'b0;               
+  case(alu_cmd)
+    // add, xor, sub, lsl, cmp
+    3'b000: // add 2 9-bit unsigned
+      rslt = inA + inB;
+	  3'b001: // left_shift
+			begin
+      // operator left shift **CANNOT LEFT SHIFT MORE THAN 9 (length of reg)**
+      	flag = rslt[9-inB];
+      	rslt = rslt << inB;
+			end
+    3'b011: // bitwise XOR
+	    rslt = inA ^ inB;
+	  3'b110: // subtract
+			begin
+	    	rslt = inA - inB;
+      	if ((inB - inA) > 0) begin
+        	flag = 'b1;
+      	end
+			end
+	  3'b111: // cmp
+      if ((inA - inB) == 0) begin
+        flag = 'b1;
+      end
+  endcase
+end
+   
+endmodule
---
+++ top_level.sv
@@ -0,0 +1,103 @@
+// sample top level design
+module top_level(
+  input        clk, reset, req, 
+  output logic done);
+  parameter D = 12,             // program counter width
+    A = 3;             		  // ALU command bit width
+  wire[D-1:0] target, 			  // jump 
+              prog_ctr;
+  
+  // RegFile
+  wire[7:0]   datA,datB,		  // from RegFile
+              mux_alu_src, 		  // ALU's 2nd source
+			  rslt,				  // alu output
+  			  wr_reg_data;        // Write Data to RegFile       
+  wire[4:0]   immed;			  // immediate value
+  
+  // PC
+  wire  relj;                     // from control to PC; relative jump enable
+  
+  // Control Outputs
+  logic UncdJmp,
+  		RdMem,
+  		WrMem,
+  		JType,
+  		IType,
+  		WrReg,
+  		Movf;
+  logic[2:0] ALUOp;
+
+  // Register File
+	logic[7:0] dat_out,
+						 dat_acc_out,
+					   dat_flag_out;
+  
+  wire[A-1:0] alu_cmd;
+  wire[8:0]   mach_code;          // machine code
+
+	// Data Memory
+	logic[7:0] mem_data;
+  
+  wire[2:0] rd_addr, wr_addr;
+// fetch subassembly
+  PC #(.D(D)) 					  // D sets program counter width
+     pc1 (.reset            ,
+         .clk              ,
+		 .reljump_en (relj),
+		 .absjump_en (absj),
+		 .target           ,
+		 .prog_ctr          );
+
+// lookup table to facilitate jumps/branches
+  PC_LUT #(.D(D))
+    pl1 (.addr  (how_high),
+         .target          );   
+
+// contains machine code
+  instr_ROM ir1(.prog_ctr,
+               .mach_code);
+
+// control decoder ****************** FIX
+  Control ctl1(.instr(),
+               .UncdJmp,
+               .RdMem,
+               .WrMem,
+               .JType,
+               .IType,
+               .WrReg,
+               .Movf,
+               .ALUOp);
+
+  assign rd_addr = mach_code[4:1];
+  assign alu_cmd  = mach_code[8:5];
+  assign immed = mach_code[4:0]; // ******** Might need to be sign extended *****
+  assign wr_addr = (Movf) ? rd_addr : 0; // decides destination reg between operand reg (for movf) & R0
+  assign wr_reg_data = (RdMem) ? mem_data : rslt; // decides between Memory Data (Ld) and ALU Result (all other instructions)
+  
+	reg_file #(.pw(3)) rf1(
+      .dat_in(wr_reg_data),	   // loads, most ops
+      .wr_en(WrReg),
+		.rd_addr,
+		.wr_addr,
+		.dat_out, // operand register data
+		.dat_acc_out, // accumulator register data
+		.dat_flag_out // status register data
+	);
+  
+  assign mux_alu_src = IType ? immed : dat_out; // decides ALU 2nd source between immediate value and operand register data
+
+  alu alu1(.alu_cmd(),
+         .inA    (datA),
+		 .inB    (muxB),
+		 .rslt       
+     ); 
+
+  dat_mem dm1(.dat_in(dat_acc_out)  ,  // the write data is in R0
+             .clk           ,
+			 .wr_en  (WrMem), // stores
+              .addr   (dat_out), // address is operand register
+             .mem_data);
+
+  assign done = prog_ctr == 128;
+ 
+endmodule
